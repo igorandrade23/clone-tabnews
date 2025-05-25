50c59c6a9c5a2d831c4772a53cf144001bd0bea9
igor <buticoazu@hotmail.com>
2025-05-25 17:45:29 -0300

    add api/v1/migrations

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,37 +1,35 @@
-import database from "infra/database.js";
+import database from "infra/database";
 
 async function status(request, response) {
   const updatedAt = new Date().toISOString();
-  const postgresVersionResult = await database.query("SHOW server_version;");
-  const postgresVersionValue = postgresVersionResult?.rows?.[0]?.server_version;
 
-  const postgresMaxConectionsResult = await database.query(
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+  const databaseMaxConnectionsResult = await database.query(
     "SHOW max_connections;"
   );
-  const postgresMaxConectionsValue =
-    postgresMaxConectionsResult?.rows?.[0]?.max_connections;
+  const databaseMaxConnectionsValue =
+    databaseMaxConnectionsResult.rows[0].max_connections;
 
   const databaseName = process.env.POSTGRES_DB;
-
-  const postgresOpenConectionsResult = await database.query({
-    text: `SELECT count(*)::int from pg_stat_activity WHERE datname = $1;`,
+  const databaseOpenedConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
     values: [databaseName],
   });
-  const postgresOpenConectionsValue =
-    postgresOpenConectionsResult?.rows?.[0]?.count;
+  const databaseOpenedConnectionsValue =
+    databaseOpenedConnectionsResult.rows[0].count;
 
-  const responseBody = {
+  response.status(200).json({
+    updated_at: updatedAt,
     dependencies: {
       database: {
-        version: postgresVersionValue,
-        max_conections: parseInt(postgresMaxConectionsValue),
-        open_conections: postgresOpenConectionsValue,
+        version: databaseVersionValue,
+        max_connections: parseInt(databaseMaxConnectionsValue),
+        opened_connections: databaseOpenedConnectionsValue,
       },
     },
-    updated_at: updatedAt,
-  };
-
-  response.status(200).json(responseBody);
+  });
 }
 
 export default status;
