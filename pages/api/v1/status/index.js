abbf608aabbb4f33644ae02fdc258b8e7d2e8689
igor <buticoazu@hotmail.com>
2025-05-25 00:33:28 -0300

    status agora é funcional

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,38 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ chave: "são acima da média" });
+  const updatedAt = new Date().toISOString();
+  const postgresVersionResult = await database.query("SHOW server_version;");
+  const postgresVersionValue = postgresVersionResult?.rows?.[0]?.server_version;
+
+  const postgresMaxConectionsResult = await database.query(
+    "SHOW max_connections;"
+  );
+  const postgresMaxConectionsValue =
+    postgresMaxConectionsResult?.rows?.[0]?.max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  console.log(`banco de dados: ${databaseName}`);
+
+  const postgresOpenConectionsResult = await database.query({
+    text: `SELECT count(*)::int from pg_stat_activity WHERE datname = $1;`,
+    values: [databaseName],
+  });
+  const postgresOpenConectionsValue =
+    postgresOpenConectionsResult?.rows?.[0]?.count;
+
+  const responseBody = {
+    dependencies: {
+      database: {
+        version: postgresVersionValue,
+        max_conections: parseInt(postgresMaxConectionsValue),
+        open_conections: postgresOpenConectionsValue,
+      },
+    },
+    updated_at: updatedAt,
+  };
+
+  response.status(200).json(responseBody);
 }
 
 export default status;
