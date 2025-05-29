34031139f0c8f9b9664610f7cf1343866887baa1
igor <buticoazu@hotmail.com>
2025-05-28 23:42:02 -0300

    feat: create `user` models `v1/users` controller with tests

--- models/migrator.js
+++ models/migrator.js
@@ -5,7 +5,7 @@ import database from "infra/database.js";
 const defaultMigrationOptions = {
   dir: resolve("infra", "migrations"),
   direction: "up",
-  verbose: true,
+  log: () => {},
   migrationsTable: "pgmigrations",
 };
 
